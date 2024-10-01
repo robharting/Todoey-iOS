a3b51686ed404a96723545e7bc70dc24d7e6bb34
Rob Harting <robharting@hotmail.com>
2024-10-01 08:17:15 +0200

    store array in defaults

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,8 +12,14 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["find Mike", "Buy Eggs", "Destroy Demo"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         
         
     }
@@ -59,6 +65,9 @@ class TodoListViewController: UITableViewController {
             // what will happen once the user clicks the Add Item on our UIAlert
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
             
             
