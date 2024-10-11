6d70ba9ada464b8dc8ba304efb5ffe846b085a6f
Rob Harting <robharting@hotmail.com>
2024-10-11 16:38:48 +0200

    fix serach and show sorted on date via added date property in db

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -87,6 +87,7 @@ class TodoListViewController: UITableViewController {
                     try self.realm.write {
                         let newItem = Item()
                         newItem.title = textField.text!
+                        newItem.dateCreated = Date()
                         currentCategory.items.append(newItem)
                         
                         //self.realm.add(newItem)
@@ -115,40 +116,34 @@ class TodoListViewController: UITableViewController {
         
         todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
         
-        
         tableView.reloadData()
     }
-    
-    
     
 }
 
 
 // MARK: - Search bar methods
-//extension TodoListViewController: UISearchBarDelegate {
-//
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        let request : NSFetchRequest<Item> = Item.fetchRequest()
-//
-//        // your query via NSPredicate
-//        let  predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-//
-//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-//
-//        loadItems(with: request, predicate: predicate)
-//    }
-//
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        if searchBar.text?.count == 0 {
-//            loadItems()
-//
-//            DispatchQueue.main.async {
-//                searchBar.resignFirstResponder()
-//            }
-//
-//        }
-//    }
-//}
+extension TodoListViewController: UISearchBarDelegate {
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+     
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        
+        tableView.reloadData()
+    }
+    
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+
+        }
+    }
+}
 
 
 
--- Todoey/DataModel/Item.swift
+++ Todoey/DataModel/Item.swift
@@ -11,7 +11,8 @@ import RealmSwift
 
 class Item: Object {
     @objc dynamic var title: String = ""
-    @objc dynamic  var done: Bool = false
+    @objc dynamic var done: Bool = false
+    @objc dynamic var dateCreated: Date?
     // define the inverse relationsship
     var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
